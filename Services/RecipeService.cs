01f6a6593a18196a2a7cea34d830432cfd56dc68
benjamin burton <benjamin.alexander.burton@gmail.com>
2025-02-28 12:41:40 +0100

    GetAllRecipesAsync() metod

---
+++ Services/RecipeService.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ReceptHemsida.Data;
+using ReceptHemsida.Models;
+
+namespace ReceptHemsida.Services
+{
+    public class RecipeService
+    {
+        // Property
+        private readonly ApplicationDbContext _context;
+
+        // Constructor
+        public RecipeService(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // Methods
+        public async Task<List<Recipe>> GetAllRecipesAsync()
+        {
+            return await _context.Recipes
+                .Include(r => r.User)
+                .Include(r => r.RecipeIngredients)
+                .Include(r => r.Comments)
+                .Include(r => r.Favorites)
+                .ToListAsync();
+        }
+    }
+}
