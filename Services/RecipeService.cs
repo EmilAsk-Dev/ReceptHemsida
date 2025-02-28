8fb9d45fbc7c34a4cf6f77947f745cb4fc7628f1
EmilAsk-Dev <carl.emil.ask1@gmail.com>
2025-02-28 17:05:29 +0100

    Added Services

--- Services/RecipeService.cs
+++ Services/RecipeService.cs
@@ -6,16 +6,16 @@ namespace ReceptHemsida.Services
 {
     public class RecipeService
     {
-        // Property
+        
         private readonly ApplicationDbContext _context;
 
-        // Constructor
+        
         public RecipeService(ApplicationDbContext context)
         {
             _context = context;
         }
 
-        // Methods
+        
         public async Task<List<Recipe>> GetAllRecipesAsync()
         {
             return await _context.Recipes
