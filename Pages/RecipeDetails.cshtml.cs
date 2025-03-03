cdc5cda5a0ed11e20b6358f10c4273d13efd42aa
Jessicabii3 <159479108+Jessicabii3@users.noreply.github.com>
2025-03-03 20:49:00 +0100

    Register layouten

---
+++ Pages/RecipeDetails.cshtml.cs
@@ -0,0 +1,31 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using ReceptHemsida.Services;
+using ReceptHemsida.Models;
+using System.Threading.Tasks;
+
+
+namespace ReceptHemsida.Pages
+{
+    public class RecipeDetailsModel : PageModel
+    {
+        private readonly RecipeService _recipeService;
+        public RecipeDetailsModel(RecipeService recipeService)
+        {
+            _recipeService = recipeService;
+        }
+        public Recipe Recipe { get; set; }
+
+        public async Task OnGetAsync(Guid id)
+        {
+            Recipe = await _recipeService.GetRecipeByIdAsync(id);
+            if(Recipe == null)
+            {
+                RedirectToPage("/Index");
+            }
+        }
+        public void OnGet()
+        {
+        }
+    }
+}
